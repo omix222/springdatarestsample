fff7cac7026bf7fb9ef20b46d66bae616ec03650
Hiromi Takahashi <hiromix222@gmail.com>
2019-12-21 17:45:15 +0900

    Spring DataREST対応

--- src/main/java/com/example/springdatarest/StudentRepository.java
+++ src/main/java/com/example/springdatarest/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.example.springdatarest;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource
+        (collectionResourceRel = "students", path = "students")
 public interface StudentRepository extends PagingAndSortingRepository<Student,String> {
 
     Iterable<Student> findAll();
